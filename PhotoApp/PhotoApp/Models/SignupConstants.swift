81395383bb12a4d1061af19df29a2a59c21413b5
vfil <wladyslawfil@gmail.com>
2023-04-28 21:43:13 +0300

    Create SignupConstants

---
+++ PhotoApp/PhotoApp/Models/SignupConstants.swift
@@ -0,0 +1,18 @@
+//
+//  SignupConstants.swift
+//  PhotoApp
+//
+//  Created by  Vladyslav Fil on 28.04.2023.
+//
+
+import Foundation
+
+struct SignupConstants {
+    static let firstNameMinLength = 2
+    static let firstNameMaxLength = 10
+    static let lastNameMinLength = 2
+    static let lastNameMaxLength = 12
+    static let passwordMinLength = 8
+    static let passwordMaxLength = 16
+    static let signupURLString = "http://appsdeveloperblog.com:8080/signup-mock-service/users"
+}
