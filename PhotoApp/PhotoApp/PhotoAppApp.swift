471f30fea9beee5e66390e33c9fbd2727c503a1d
vfil <wladyslawfil@gmail.com>
2023-04-28 21:40:53 +0300

    Create projects

---
+++ PhotoApp/PhotoApp/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by  Vladyslav Fil on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
